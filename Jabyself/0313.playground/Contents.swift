be974925e8f51248c2353376bc7dc7bb1519c4a1
JABY <devjaby@gmail.com>
2024-03-13 10:36:42 +0900

    upload

---
+++ Jabyself/0313.playground/Contents.swift
@@ -0,0 +1,35 @@
+let number: Int = 10
+let pi: Double = 3.14
+let greeting: String = "Hello, world!"
+let isLoggedIn: Bool = true
+
+var age = 20
+age = 21
+
+let name = "John"
+//name = "Jane" // 에러 발생
+
+var numbers: [Int] = [1, 2, 3, 4, 5]
+numbers.append(6)
+
+var person: [String: String] = ["name": "John", "age": "20"]
+person["age"] = "21"
+
+if age >= 21 {
+    print("You can drink alcohol.")
+} else {
+    print("You cannot drink alcohol.")
+}
+
+for i in 1...5 {
+    print(i)
+}
+
+var count = 0
+while count < 10 {
+    count += 1
+    print(count)
+}
+  
+
+
