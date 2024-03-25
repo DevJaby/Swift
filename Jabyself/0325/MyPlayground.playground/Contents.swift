e1eb7a52cab2625970818d0e4f9022e7b768884d
JABY <devjaby@gmail.com>
2024-03-25 22:14:28 +0900

    upload

---
+++ Jabyself/0325/MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+let digit: Character = "9"
+let intDigit = Int(String(digit))
+
+let strNumber = String(digit)
+let intNumber = Int(strNumber)
+
+let name = "박우찬"
+
+for char in name {
+    print(char)
+}
+
