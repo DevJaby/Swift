92d61e376d4c298f51db26f550c3b328c446efe3
JABY <devjaby@gmail.com>
2024-03-26 11:31:27 +0900

    upload

---
+++ Programmers_Swift/0326/가운데 글자 가져오기.playground/Contents.swift
@@ -0,0 +1,18 @@
+/*:# 가운데 글자 가져오기*/
+
+func solution (_ s: String) -> String {
+    let length = s.count
+    let middleIndex = length / 2
+    
+    if length % 2 == 0 {
+        //짝수인 경우
+        return String(s[s.index(s.startIndex, offsetBy: middleIndex - 1)...s.index(s.startIndex, offsetBy: middleIndex)])
+    } else {
+        //홀수인 경우
+        return String(s[s.index(s.startIndex, offsetBy: middleIndex)])
+    }
+    
+}
+
+print(solution("abcde"))
+
